dbafc0e41779ba162ebd92eb5578844a8a0b5043
CaoToan <caotoan940@gmail.com>
2023-09-18 00:58:16 +0200

    optimize code: mixin + helper

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,39 +1,11 @@
 const Product = require("../../models/product.model");
+const filterStatusHeper = require("../../helpers/filterStatus");
+const searchHeper = require("../../helpers/search");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-
-    let filterStatus = [
-        {
-            name: "All",
-            status: "",
-            class: ""
-        },
-        {
-            name: "Active",
-            status: "active",
-            class: ""
-        },
-        {
-            name: "Inactive",
-            status: "inactive",
-            class: ""
-        }
-    ];
-
-    if (req.query.status) {
-        const index = filterStatus.findIndex(item => {
-            return item.status == req.query.status;
-        });
-
-        filterStatus[index].class = "active";
-    } else {
-        const index = filterStatus.findIndex(item => {
-            return item.status == "";
-        });
-
-        filterStatus[index].class = "active";
-    }
+    const filterStatus = filterStatusHeper(req.query);
+    let objectSearch = searchHeper(req.query);
 
     let findConditions = {
         deleted: false
@@ -43,14 +15,9 @@ module.exports.index = async (req, res) => {
         findConditions.status = req.query.status;
     }
 
-    let keyword = "";
     if (req.query.keyword) {
-        keyword = req.query.keyword;
-
-        const regex = new RegExp(keyword, "i");
-        findConditions.title = regex;
+        findConditions.title = objectSearch.regex;
     }
-
     const products = await Product.find(findConditions);
 
 
@@ -58,6 +25,6 @@ module.exports.index = async (req, res) => {
         pageTitle: "Product Catalog",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objectSearch.keyword
     });
 }
