6670de5cc62db7ab53db27c6160acf869a8f66f1
CaoToan <caotoan940@gmail.com>
2023-10-07 10:33:19 +0200

    Finished Add Category for Products

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,8 +1,10 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
 const systemConfig = require("../../config/system");
+const createTree = require("../../helpers/createTree");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -129,9 +131,18 @@ module.exports.deleteItem = async (req, res) => {
 };
 
 // [GET] /admin/products/create
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+    let find = {
+        deleted: false
+    }
+
+    const records = await ProductCategory.find(find);
+
+    const newRecords = createTree(records);
+
     res.render("admin/pages/products/create", {
-        pageTitle: "Add new product"
+        pageTitle: "Add new product",
+        records: newRecords
     });
 };
 
@@ -168,8 +179,15 @@ module.exports.edit = async (req, res) => {
             deleted: false
         });
 
+        const records = await ProductCategory.find({
+            deleted: false
+        });
+
+        const newRecords = createTree(records);
+
         res.render("admin/pages/products/edit", {
             product: product,
+            records: newRecords,
             pageTitle: "Edit Product"
         });
     } catch (error) {
