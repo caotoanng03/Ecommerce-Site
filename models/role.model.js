3c3100d0d00484447f95de3a91d6405571f7fd1d
CaoToan <caotoan940@gmail.com>
2023-10-07 11:00:20 +0200

    Add Role model + collection roles

---
+++ models/role.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongose');
+
+const roleSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    permissions: {
+        type: Array,
+        default: []
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date,
+}, { timestamps: true });
+
+const Role = mongoose.model("Role", roleSchema, "roles");
+
+module.exports = Role;
