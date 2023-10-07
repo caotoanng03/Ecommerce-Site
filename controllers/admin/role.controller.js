764bf9a3b8a833f6d5d05936249a6f42b7a42382
CaoToan <caotoan940@gmail.com>
2023-10-07 12:11:17 +0200

    Finished Editing Role Group

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -16,7 +16,7 @@ module.exports.index = async (req, res) => {
 // [GET] /admin/roles/create
 module.exports.create = async (req, res) => {
     res.render("admin/pages/roles/create", {
-        pageTitle: "Tạo mới nhóm quyền",
+        pageTitle: "New Role Group",
     });
 };
 
@@ -28,4 +28,33 @@ module.exports.createPost = async (req, res) => {
     req.flash("success", "One role group was added");
 
     res.redirect(`/${systemConfig.prefixPathAdmin}/roles`);
+};
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const data = await Role.findOne({
+            _id: id,
+            deleted: false
+        });
+
+        res.render("admin/pages/roles/edit", {
+            data: data,
+            pageTitle: "Edit Role Group"
+        });
+    } catch (error) {
+        res.redirect(`/${systemConfig.prefixPathAdmin}/roles`);
+    }
+};
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+
+    await Role.updateOne({ _id: id }, req.body);
+
+    req.flash("success", "The role group was updated");
+    res.redirect(`back`);
 };
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -10,5 +10,10 @@ router.get('/create', controller.create);
 
 router.post('/create', controller.createPost);
 
+router.get('/edit/:id', controller.edit);
+
+router.patch('/edit/:id', controller.editPatch);
+
+
 
 module.exports = router;
