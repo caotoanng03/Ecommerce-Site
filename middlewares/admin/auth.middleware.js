4d7efb59577758bcfe0f226471ae90c4f6721b29
CaoToan <caotoan940@gmail.com>
2023-10-14 08:53:04 +0200

    authorization  interface

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,4 +1,5 @@
 const Account = require('../../models/account.model');
+const Role = require('../../models/role.model');
 const systemConfig = require('../../config/system');
 
 module.exports.requireAuth = async (req, res, next) => {
@@ -16,5 +17,11 @@ module.exports.requireAuth = async (req, res, next) => {
         return;
     }
 
+    const role = await Role.findOne({
+        _id: user.role_id
+    }).select('title permissions');
+
+    res.locals.user = user;
+    res.locals.role = role;
     next();
 };
