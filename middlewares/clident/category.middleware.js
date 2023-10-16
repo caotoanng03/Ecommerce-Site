c2f6535d3fd4861e71299d80e7005e33ed4ef10f
CaoToan <caotoan940@gmail.com>
2023-10-16 20:58:21 +0200

    Finished Adding outstanding feature product on admin

--- middlewares/clident/category.middleware.js
+++ middlewares/clident/category.middleware.js
@@ -6,7 +6,7 @@ module.exports.category = async (req, res, next) => {
     const categoryProducts = await ProductCategory.find({
         deleted: false
     });
-    console.log(categoryProducts);
+
     const newCategoryProducts = createTree(categoryProducts);
 
     res.locals.layoutCategoryProducts = newCategoryProducts;
