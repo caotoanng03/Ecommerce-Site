122e9fc1081eb2b21376ae8a84f7dfc06cab0cb1
CaoToan <caotoan940@gmail.com>
2023-10-20 09:53:36 +0200

    Cart page

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,38 @@
 const Cart = require('../../models/cart.model');
+const Product = require('../../models/product.model');
+
+const productHelper = require('../../helpers/product');
+
+// [GET] /cart
+module.exports.index = async (req, res) => {
+    const cartId = req.cookies.cartId;
+
+    const cart = await Cart.findOne({
+        _id: cartId
+    })
+
+    if (cart.products.length > 0) {
+        for (const item of cart.products) {
+            const productId = item.product_id;
+
+            const productInfo = await Product.findOne({
+                _id: productId
+            });
+
+            productInfo.newPrice = productHelper.newPriceProduct(productInfo);
+
+            item.productInfo = productInfo;
+            item.totalPrice = productInfo.newPrice * item.quantity;
+        }
+    }
+
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0);
+
+    res.render('client/pages/cart/index', {
+        pageTitle: 'Cart',
+        cartDetail: cart,
+    });
+};
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
