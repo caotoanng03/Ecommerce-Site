2221a31f4ad3e442b95c599a1015ee74f9bbba45
CaoToan <caotoan940@gmail.com>
2023-10-20 09:00:24 +0200

    Mini cart on Header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -12,6 +12,22 @@ module.exports.cartId = async (req, res, next) => {
     } else {
         // khi da co gio hang
         // kiem tra cartId đúng hay không, không đúng thì tạo lại mới
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        });
+
+        if (cart) {
+            cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
+            res.locals.miniCart = cart;
+        } else {
+            const cart = new Cart();
+            await cart.save();
+
+            oneYearExpries = 1000 * 60 * 60 * 24 * 365;
+            res.cookie('cartId', cart.id, {
+                expires: new Date(Date.now() + oneYearExpries)
+            });
+        }
     }
 
     next();
