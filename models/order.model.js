4ef5307e02eb9bfc606b95159a9f1604c304b76b
CaoToan <caotoan940@gmail.com>
2023-10-21 09:40:36 +0200

    Add order model

---
+++ models/order.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const orderShema = mongoose.Schema({
+    // user_id: String,
+    cart_id: String,
+    userInfo: {
+        fullName: String,
+        phone: String,
+        address: String
+    },
+    products: [
+        {
+            product_id: String,
+            price: Number,
+            discountPercentage: Number,
+            quantity: Number
+        }
+    ]
+}, { timestamps: true });
+
+const Order = mongoose.model('Order', orderShema, 'orders');
+
+module.exports = Order;
