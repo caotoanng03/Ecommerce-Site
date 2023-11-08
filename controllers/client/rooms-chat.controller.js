c7be297f69bda301f2aa6b62b40beea8a040089b
CaoToan <caotoan940@gmail.com>
2023-11-08 22:15:43 +0100

    Create Room Chat Feature

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -1,9 +1,58 @@
 const RoomChat = require("../../models/rooms-chat.model");
+const User = require("../../models/user.model");
 
 // [GET] /rooms-chat
 module.exports.index = async (req, res) => {
     res.render("client/pages/rooms-chat/index", {
         pageTitle: "Room-Chat"
     });
+};
+
+// [GET] /rooms-chat/create
+module.exports.create = async (req, res) => {
+    const friendList = res.locals.user.friendList;
+
+    for (const friend of friendList) {
+        const infoFriend = await User.findOne({
+            _id: friend.user_id
+        }).select("fullName avatar");
+
+        friend.infoFriend = infoFriend;
+    }
+
+    res.render("client/pages/rooms-chat/create", {
+        pageTitle: "Create Room Chat",
+        friendList
+    });
+};
+
+// [POST] /rooms-chat/createPost
+module.exports.createPost = async (req, res) => {
+    const title = req.body.title;
+    const usersId = req.body.usersId;
+    const myId = res.locals.user.id;
+
+    const dataRoomChat = {
+        title: title,
+        typeRoom: "group",
+        users: []
+    };
+
+    usersId.forEach(userId => {
+        dataRoomChat.users.push({
+            user_id: userId,
+            role: "user"
+        });
+    });
+
+    dataRoomChat.users.push({
+        user_id: myId,
+        role: "superAdmin"
+    });
+
+    const room = new RoomChat(dataRoomChat);
+    await room.save();
+
+    res.redirect(`/chat/${room.id}`);
 };
 
--- routes/client/rooms-chat.route.js
+++ routes/client/rooms-chat.route.js
@@ -5,4 +5,8 @@ const controller = require("../../controllers/client/rooms-chat.controller");
 
 router.get("/", controller.index);
 
+router.get("/create", controller.create);
+
+router.post("/create", controller.createPost);
+
 module.exports = router;
